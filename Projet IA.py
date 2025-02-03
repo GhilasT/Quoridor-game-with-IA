cdd3dccbc7f2f35115053ac8692de8defa00235e
Ghilas Tidjet <ghilastidjet7@gmail.com>
2025-02-03 18:27:11 +0100

    Added window

--- Projet IA.py
+++ Projet IA.py
@@ -6,3 +6,6 @@ pygame.init()
 # Définir les dimensions de la fenêtre
 largeur, hauteur = 900, 900
 taille_case = (largeur) // 9
+
+# Créer la fenêtre
+fenetre = pygame.display.set_mode((largeur, hauteur))
