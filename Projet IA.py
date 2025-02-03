b8fde4ac47d6b0ccad287df15a66fc8af85dac7a
Ghilas Tidjet <ghilastidjet7@gmail.com>
2025-02-03 18:55:38 +0100

    correction de la taille des cases pour eviter l'overflow des cases

--- Projet IA.py
+++ Projet IA.py
@@ -6,8 +6,8 @@ pygame.init()
 
 # Définir les dimensions de la fenêtre
 largeur, hauteur = 900, 900
-taille_case = (largeur) // 9
 marge = 50
+taille_case = (largeur - 2*marge) // 9
 
 # Créer la fenêtre
 fenetre = pygame.display.set_mode((largeur, hauteur))
